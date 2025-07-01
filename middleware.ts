ce82678ce9eb38d7c4e06a3b79e1f6d4c231eebf
kayp514 <pa.ka12@outlook.com>
2025-07-01 14:26:00 +0000

    Restrict allowed origins to specific domain for CORS configuration

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server'
  
-const allowedOrigins = ['*']
+const allowedOrigins = ['https://ternauth-test.vercel.app/']
  
 const corsOptions = {
   'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
